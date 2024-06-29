190c7d6895ec4cc9656bb8389da7396428a48574
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-29 16:40:48 -0700

    Add brand schema

--- models/brand.js
+++ models/brand.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+  name: { type: String, required: true },
+  website: { type: String, required: true },
+  // Supplier(s)?
+});
+
+// Export model
+module.exports = mongoose.model("Brand", BrandSchema);
