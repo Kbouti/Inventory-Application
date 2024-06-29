10265000da5fccc0715a79578789dca1d8f7859b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-29 16:38:26 -0700

    Write schema for part model

--- models/part.js
+++ models/part.js
@@ -2,4 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const PartSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  brand: { type: Schema.Types.ObjectId, ref: "Brand", required: true }, // reference to the associated brand
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  // Supplier?
+});
 
+// Export model
+module.exports = mongoose.model("Part", PartSchema);
