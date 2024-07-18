543c1d772bf4867da53dadabf16673bb8f86cee9
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-18 13:34:21 -0700

    Remove required status for part description

--- models/part.js
+++ models/part.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const PartSchema = new Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String},
   brand: { type: Schema.Types.ObjectId, ref: "Brand", required: true }, // reference to the associated brand
   price: { type: Number, required: true, min: 0.01},
   quantity: { type: Number, required: true },
