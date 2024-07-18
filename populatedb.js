b1fd1cd43e8bc1c3a3d8e015ad09d0a171c49f22
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-18 14:04:11 -0700

    Update comments and logs

--- populatedb.js
+++ populatedb.js
@@ -36,7 +36,7 @@ async function main() {
   mongoose.connection.close();
 }
 
-//  My attempt at create brand function:
+//  Create brand function:
 async function brandCreate(index, name, website, supplier) {
   const brandDetail = { name: name, website: website };
   if (suppliers != false) brandDetail.suppliers = suppliers;
@@ -46,7 +46,7 @@ async function brandCreate(index, name, website, supplier) {
   console.log(`Added brand: ${name}`);
 }
 
-//   Attempt at category function
+//   Create category function
 async function categoryCreate(index, name) {
   const category = new Category({ name: name });
   await category.save();
@@ -54,35 +54,8 @@ async function categoryCreate(index, name) {
   console.log(`Added category: ${name} _id: ${category._id}`);
 }
 
-// ******************************************************************************************************************************************************************
-// NOTE:
 
-// Need to add to part:
-// -Category
-// -SKU
-// -link
-// -image
-// -**Fix link to suppliers object
-
-// ******************************************************************************************************************************************************************
-// ************************************************************************************
-// We've broken something in the process of updating our parts.
-// Getting a message: "Part validation failed"
-
-// The code runs fine at commit titled "Update Objective notes" but errors at "Add category field to parts".
-// That's where we need to look for mistakes
-
-// ********* It's getting an array instead of a category *******
-// It must be confusing suppliers with category? Or just passing category as an array for some reason. I don't think it's looking at suppliers 
-// It seems to be accurately locating category "fork"
-// Perhaps I could change the order of the arguments so that anything not required is at the end? It shouldn't matter.....
-
-// But yeah it seems like the problem occurs while running main()
-// It is registering the part's category as an array. It shouldn't be an array. 
-
-// ************************************************************************************
-
-//   Attempted part function
+//   Create part function
 async function partCreate(
   index,
   name,
@@ -102,11 +75,6 @@ async function partCreate(
     category: category,
     imageUrl: imageUrl,
   };
-
-  console.log(`Adding a new part: ${name}`);
-  // console.log(`part category: ${category}`);
-  // console.log(`part suppliers: ${suppliers});
-  // ^^These logs appear normal
 
   if (description != false) {
     partDetail.description = description;
@@ -197,6 +165,7 @@ async function createSuppliers() {
   ]);
 }
 
+// Finally create parts
 async function createParts() {
   console.log(`Adding parts`);
   await Promise.all([
@@ -236,148 +205,3 @@ async function createParts() {
     ),
   ]);
 }
-
-//  ********************************************************************************************************
-//  Template code below this line
-//  ********************************************************************************************************
-
-// async function createGenres() {
-//   console.log("Adding genres");
-//   await Promise.all([
-//     genreCreate(0, "Fantasy"),
-//     genreCreate(1, "Science Fiction"),
-//     genreCreate(2, "French Poetry"),
-//   ]);
-// }
-
-// async function createAuthors() {
-//   console.log("Adding authors");
-//   await Promise.all([
-//     authorCreate(0, "Patrick", "Rothfuss", "1973-06-06", false),
-//     authorCreate(1, "Ben", "Bova", "1932-11-8", false),
-//     authorCreate(2, "Isaac", "Asimov", "1920-01-02", "1992-04-06"),
-//     authorCreate(3, "Bob", "Billings", false, false),
-//     authorCreate(4, "Jim", "Jones", "1971-12-16", false),
-//   ]);
-// }
-
-// async function createBooks() {
-//   console.log("Adding Books");
-//   await Promise.all([
-//     bookCreate(
-//       0,
-//       "The Name of the Wind (The Kingkiller Chronicle, #1)",
-//       "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
-//       "9781473211896",
-//       authors[0],
-//       [genres[0]]
-//     ),
-//     bookCreate(
-//       1,
-//       "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-//       "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
-//       "9788401352836",
-//       authors[0],
-//       [genres[0]]
-//     ),
-//     bookCreate(
-//       2,
-//       "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-//       "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
-//       "9780756411336",
-//       authors[0],
-//       [genres[0]]
-//     ),
-//     bookCreate(
-//       3,
-//       "Apes and Angels",
-//       "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
-//       "9780765379528",
-//       authors[1],
-//       [genres[1]]
-//     ),
-//     bookCreate(
-//       4,
-//       "Death Wave",
-//       "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-//       "9780765379504",
-//       authors[1],
-//       [genres[1]]
-//     ),
-//     bookCreate(
-//       5,
-//       "Test Book 1",
-//       "Summary of test book 1",
-//       "ISBN111111",
-//       authors[4],
-//       [genres[0], genres[1]]
-//     ),
-//     bookCreate(
-//       6,
-//       "Test Book 2",
-//       "Summary of test book 2",
-//       "ISBN222222",
-//       authors[4],
-//       false
-//     ),
-//   ]);
-// }
-
-// async function createBookInstances() {
-//   console.log("Adding authors");
-//   await Promise.all([
-//     bookInstanceCreate(
-//       0,
-//       books[0],
-//       "London Gollancz, 2014.",
-//       false,
-//       "Available"
-//     ),
-//     bookInstanceCreate(1, books[1], " Gollancz, 2011.", false, "Loaned"),
-//     bookInstanceCreate(2, books[2], " Gollancz, 2015.", false, false),
-//     bookInstanceCreate(
-//       3,
-//       books[3],
-//       "New York Tom Doherty Associates, 2016.",
-//       false,
-//       "Available"
-//     ),
-//     bookInstanceCreate(
-//       4,
-//       books[3],
-//       "New York Tom Doherty Associates, 2016.",
-//       false,
-//       "Available"
-//     ),
-//     bookInstanceCreate(
-//       5,
-//       books[3],
-//       "New York Tom Doherty Associates, 2016.",
-//       false,
-//       "Available"
-//     ),
-//     bookInstanceCreate(
-//       6,
-//       books[4],
-//       "New York, NY Tom Doherty Associates, LLC, 2015.",
-//       false,
-//       "Available"
-//     ),
-//     bookInstanceCreate(
-//       7,
-//       books[4],
-//       "New York, NY Tom Doherty Associates, LLC, 2015.",
-//       false,
-//       "Maintenance"
-//     ),
-//     bookInstanceCreate(
-//       8,
-//       books[4],
-//       "New York, NY Tom Doherty Associates, LLC, 2015.",
-//       false,
-//       "Loaned"
-//     ),
-//     bookInstanceCreate(9, books[0], "Imprint XXX2", false, false),
-//     bookInstanceCreate(10, books[1], "Imprint XXX3", false, false),
-//   ]);
-// }
