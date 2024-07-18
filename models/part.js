a52cc06d43ab5b677c5e2cacc367fbcc8456ba53
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-07-18 13:39:53 -0700

    Provide message for each required field

--- models/part.js
+++ models/part.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PartSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: [true, "Name required"] },
   description: { type: String},
-  brand: { type: Schema.Types.ObjectId, ref: "Brand", required: true }, // reference to the associated brand
+  brand: { type: Schema.Types.ObjectId, ref: "Brand", required: [true, "Brand is required"] }, // reference to the associated brand
   price: { type: Number, required: true, min: 0.01},
-  quantity: { type: Number, required: true },
+  quantity: { type: Number, required: [true, "Quantity is required"] },
   suppliers: [{ type: Schema.Types.ObjectId, ref: "Supplier" }],
-  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-  imageUrl: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: [true, "A category is required"] },
+  imageUrl: { type: String, required: [true, "Image is required"] },
 
 });
 
