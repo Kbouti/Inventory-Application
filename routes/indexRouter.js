96ac336091aa90f3c09afb702661d22a4eb0e143
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-05 16:19:01 -0700

    Establish title and subTitle parameters

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,35 +5,46 @@ const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
 
+// ************************************************************************************
+// These declarations might be best in app.js, not sure
 
 const links = [
-    { href: "/", text: "Home" },
-    { href: "new", text: "New Category" },
-]
+  { href: "/", text: "Home" },
+  { href: "new", text: "New Category" },
+];
+const title = "Inventory Application";
 
-
+// ************************************************************************************
 
 indexRouter.get("/", async (req, res) => {
   // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
-  res.render("../views/index", { links, title: "Inventory Application" });
+  res.render("../views/index", {
+    links,
+    title,
+    subTitle: "Home",
+  });
 });
 
 indexRouter.get("/new", async (req, res) => {
-    // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
-    res.render("../views/createCategory", { links, title: "New Category" });
+  // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
+  res.render("../views/createCategory", {
+    links,
+    title,
+    subTitle: "New Category",
   });
+});
 
-  indexRouter.post("/new", async (req, res) => {
-    // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
-    res.send("You've sent the form");
-  });
-
-
-
+indexRouter.post("/new", async (req, res) => {
+  // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
+  res.send("You've sent the form");
+});
 
 indexRouter.get("/:category", async (req, res) => {
-    // Here we will eventually render all the parts in this category, as well as any forms to alter this category
-  res.render("../views/index/category", { title: "Inventory Application" });
+  // Here we will eventually render all the parts in this category, as well as any forms to alter this category
+  res.render("../views/index/category", {
+    title,
+    subTitle: "Create a Category",
+  });
 });
 
 module.exports = indexRouter;
