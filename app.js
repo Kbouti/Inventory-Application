7045d8fcb330776e841a4a786d0e1ae205c7c6ec
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-05 10:37:59 -0700

    Install ejs and render home view with indexRouter

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const app = express();
 
 require("dotenv").config();
 
+const indexRouter = require("./routes/indexRouter");
+
+
 const PORT = process.env.PORT;
 const MODE = process.env.MODE;
+
+const path = require("path");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+// Needed to serve static assets (css)
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// Needed to support ejs view engine
+
+
+app.use("/", indexRouter);
 
 
 app.listen(PORT, () =>
   console.log(`My first Express app - listening mode ${MODE} on port ${PORT}!`)
 );
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+// app.get("/", (req, res) => res.send("Hello, world!"));
+
+
+
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,3 +1,15 @@
 const { Router } = require("express");
 
 const indexRouter = Router();
+
+const indexController = require("../controllers/indexController");
+
+indexRouter.get("/", async (req, res) => {
+// Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it. 
+
+
+    res.render("../views/index", { title: "Inventory Application" });
+  });
+
+
+  module.exports = indexRouter
