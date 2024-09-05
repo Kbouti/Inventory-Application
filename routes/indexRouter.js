3ef7ad28c3c5e452725c627da6f52cb9830182a5
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-05 11:56:22 -0700

    Create navbar

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,12 +4,26 @@ const indexRouter = Router();
 
 const indexController = require("../controllers/indexController");
 
+
+
+const links = [
+    { href: "/", text: "Home" },
+    { href: "new", text: "New Category" },
+]
+
+
+
 indexRouter.get("/", async (req, res) => {
-// Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it. 
+  // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
+  res.render("../views/index", { links, title: "Inventory Application" });
+});
 
 
-    res.render("../views/index", { title: "Inventory Application" });
-  });
 
 
-  module.exports = indexRouter
+indexRouter.get("/:category", async (req, res) => {
+    // Here we will eventually render all the parts in this category, as well as any forms to alter this category
+  res.render("../views/index/category", { title: "Inventory Application" });
+});
+
+module.exports = indexRouter;
