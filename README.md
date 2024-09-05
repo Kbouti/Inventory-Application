2b8c98b37ffe032fb61988b5ebd1be32c1889b68
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-05 10:21:33 -0700

    Create index view

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Restarting project on 9/5/24 after curriculum updates
 
 https://www.theodinproject.com/lessons/node-path-nodejs-inventory-application
+
+
+run app with the command: 
+node --watch app.js
--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 
 require("dotenv").config();
 
-
-app.get("/", (req, res) => res.send("Hello, world!"));
-
 const PORT = process.env.PORT;
 const MODE = process.env.MODE;
 
+app.get("/", (req, res) => res.send("Hello, world!"));
 
-app.listen(PORT, () => console.log(`My first Express app - listening mode ${MODE} on port ${PORT}!`));
+app.listen(PORT, () =>
+  console.log(`My first Express app - listening mode ${MODE} on port ${PORT}!`)
+);
