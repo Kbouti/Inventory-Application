b416a30ee599a03252aaf0f0194dbca5f92de369
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-05 10:24:23 -0700

    Create index route and controller files

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const MODE = process.env.MODE;
 
-app.get("/", (req, res) => res.send("Hello, world!"));
 
 app.listen(PORT, () =>
   console.log(`My first Express app - listening mode ${MODE} on port ${PORT}!`)
 );
+
+app.get("/", (req, res) => res.send("Hello, world!"));
