fa453aac2a2d4a9d433e0ef9829edc540666d087
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-06 13:06:23 -0700

    Capture form submission in approprioate route

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,14 +6,25 @@ const staticResources = require("../staticResources");
 let links = staticResources.links;
 let title = staticResources.title;
 
-
-
 categoryRouter.get("/", async (req, res) => {
-    res.render("../views/index", {
-        links,
-        title,
-        subTitle: "Categories",
-      });
+  res.render("../views/categories", {
+    links,
+    title,
+    subTitle: "Categories",
+  });
+});
+
+categoryRouter.post("/new", async (req, res) => {
+  const userInput = req.body.category;
+  // ******************************************************************************************************
+  // Ok! Here is where we need middleware functions to handle the user's input.
+  // Need to validate and sanitize input
+  // Need to create a new row in the categories table for each user submitted category
+  // ******************************************************************************************************
+
+  console.log(`You've submitted the following category name: ${userInput}`);
+
+  res.redirect("/");
 });
 
 module.exports = categoryRouter;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -21,31 +21,9 @@ indexRouter.get("/", async (req, res) => {
   });
 });
 
-// indexRouter.get("/new", async (req, res) => {
-//   // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
-//   res.render("../views/createCategory", {
-//     links,
-//     title,
-//     subTitle: "New Category",
-//   });
-// });
 
-indexRouter.post("/new", async (req, res) => {
-  // Here we would likely first want to access our database to get our inventory data, then we'd pass it to the view to render it.
-  const userInput = req.body.category;
-
-  // ******************************************************************************************************
-// This no longer works because we've gotten rid of the routes surrounding this form. 
-
-  // ******************************************************************************************************
-  // Ok! Here is where we need middleware functions to handle the user's input.
-  // Need to validate and sanitize input
-  // Need to create new table in database for category parts
-  // ******************************************************************************************************
-
-  res.send(`You've submitted the following category name: ${userInput}`);
-});
-
+// *****************************************************************
+// Not sure about this part
 indexRouter.get("/:category", async (req, res) => {
   // Here we will eventually render all the parts in this category, as well as any forms to alter this category
   res.render("../views/index/category", {
