0d232fa912a1af0023704640917ac9211b510bf2
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-09 14:20:13 -0700

    Move categoryGet logic to category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,5 +3,21 @@ const queries = require("../database/queries");
 // const { body, validationResult } = require("express-validator");
 // Still unsure of how to use this to validate
 
-const staticResources = require("../staticResources");
 const indexController = require("../controllers/indexController");
+
+const staticResources = require("../staticResources");
+let links = staticResources.links;
+let title = staticResources.title;
+
+exports.categoryGet = async (req, res) => {
+  const categories = await queries.getAllCategories();
+  const tags = await queries.getAllTags();
+
+  res.render("../views/categories", {
+    links,
+    title,
+    categories,
+    tags,
+    subTitle: "Categories",
+  });
+};
