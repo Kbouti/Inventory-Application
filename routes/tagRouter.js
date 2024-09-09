575fb5801f7c1ab1a086b2e10f53bb585aa4862a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-09 14:36:24 -0700

    Remove old imports from tag router

--- routes/tagRouter.js
+++ routes/tagRouter.js
@@ -4,15 +4,6 @@ const tagRouter = Router();
 
 const tagController = require("../controllers/tagController");
 
-
-const indexController = require("../controllers/indexController");
-const staticResources = require("../staticResources");
-const queries = require("../database/queries");
-
-
-let links = staticResources.links;
-let title = staticResources.title;
-
 tagRouter.get("/", tagController.tagGet);
 
 tagRouter.post("/new", tagController.tagNewPost);
