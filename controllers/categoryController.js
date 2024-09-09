9797c2fe1f4772aa543b5ba0ba37b0c67c1eaa51
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-09 14:14:41 -0700

    Change export syntax in indexController

---
+++ controllers/categoryController.js
@@ -0,0 +1,7 @@
+const queries = require("../database/queries");
+
+// const { body, validationResult } = require("express-validator");
+// Still unsure of how to use this to validate
+
+const staticResources = require("../staticResources");
+const indexController = require("../controllers/indexController");
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,18 +4,14 @@ const queries = require("../database/queries");
 // Still unsure of how to use this to validate
 
 const staticResources = require("../staticResources");
-const indexController = require("../controllers/indexController");
-
 
 let links = staticResources.links;
 let title = staticResources.title;
 
-
-
-const indexGet = async (req, res) => {
-    const categories = await queries.getAllCategories();
-    const tags = await queries.getAllTags();
-      console.log("Fetched categories and tags");
+exports.indexGet = async (req, res) => {
+  const categories = await queries.getAllCategories();
+  const tags = await queries.getAllTags();
+  console.log("Fetched categories and tags");
   res.render("../views/index", {
     links,
     title,
@@ -23,39 +19,37 @@ const indexGet = async (req, res) => {
     tags,
     subTitle: "Home",
   });
-}
+};
 
 
 
 
-
-
-async function getCategories() {
+exports.getCategories = async () => {
   const categories = await queries.getAllCategories();
   return categories;
-}
+};
 
-async function getTags() {
+exports.getTags = async () => {
   const tags = await queries.getAllTags();
   return tags;
-}
+};
 
-async function createCategory(name) {
+exports.createCategory = async () => {
   const response = queries.newCategory(name);
   console.log(`Created new category: ${name}`);
   return;
-}
+};
 
-async function createTag(name) {
+exports.createTag = async () => {
   const response = queries.newTag(name);
   console.log(`new tag ${name} created`);
   return;
-}
-
-module.exports = {
-  getCategories,
-  getTags,
-  createTag,
-  createCategory,
-  indexGet
 };
+
+// module.exports = {
+//   getCategories,
+//   getTags,
+//   createTag,
+//   createCategory,
+//   indexGet,
+// };
