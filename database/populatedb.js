6485b8735daeedb616d1617ebd4c74d0abcbab16
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-10 15:45:25 -0700

    Save notes on many to many relationship problem

--- database/populatedb.js
+++ database/populatedb.js
@@ -43,6 +43,36 @@ CREATE TABLE IF NOT EXISTS tags (
   name VARCHAR ( 20 )
 );
 
+
+**************************************************************************************************************
+
+CREATE TABLE IF NOT EXISTS parts (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 30 ),
+  category int,
+  quantity int,
+  description varchar ( 800 ),
+  tags array,
+  foreign key (category) references categories(id),
+  foreign key (tags) references tags(id)
+);
+
+
+**************************************************************************************************************
+This^^ is close but we have an issue
+We can't use array as a data type. Instead, we need need to create a join table to represent this many to many relationship
+
+This video:
+https://www.youtube.com/watch?v=4q-keGvUnag
+The good stuff starts at 6mins
+
+https://www.youtube.com/watch?v=urTwxelK-Xc
+https://help.claris.com/en/pro-help/content/many-to-many-relationships.html
+
+**************************************************************************************************************
+
+
+
 INSERT INTO categories (name) 
 VALUES
   ('Bikes'),
