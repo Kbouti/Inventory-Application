f42b98ff361cca80f8cf81e296a88bbb77745c53
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-09 17:14:07 -0700

    Install express-validator

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,6 @@
 const queries = require("../database/queries");
 
-// const { body, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 // Still unsure of how to use this to validate
 
 const staticResources = require("../staticResources");
