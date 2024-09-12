37a453654323f0315faff5245083978c3993f343
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-12 09:27:51 -0700

    Create dropTables file

---
+++ database/dropTables.js
@@ -0,0 +1,40 @@
+const { Client } = require("pg");
+
+require("dotenv").config();
+
+const mode = process.env.MODE;
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+let database;
+if (mode == "PRODUCTION") {
+  database = process.env.PROD_DB;
+} else {
+  database = process.env.DEV_DB;
+}
+
+const connectionString = `postgresql://${user}:${password}@localhost:5432/${database}`;
+
+const SQL = `
+
+drop table partstags;
+
+drop table parts;
+
+drop table categories;
+
+drop table tags;
+
+`;
+
+async function main() {
+  console.log("Deleting all tables");
+  const client = new Client({
+    connectionString: connectionString,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
