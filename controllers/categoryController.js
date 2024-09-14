7d8a83cd1449b88e4705b7e6d9b9f900cf7f0d17
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-14 12:58:51 -0700

    Troubleshoot css reference error

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -34,30 +34,35 @@ exports.categoryGet = async (req, res) => {
 // ******************************************************************************************************
 
 exports.category_nameGet = async (req, res) => {
-
-
   // ******************************************************************************************************
   const category_name = req.params.category_name;
   console.log(`category_name: ${category_name}`);
   // What the FFF this is logging style.css
   // And then the page loads like it should but with no css?????
-// Something very wrong here
+  // Something very wrong here
+
+
+  // Fixed the css issue! just need to reword our reference
   // ******************************************************************************************************
 
   const categories = await queries.getAllCategories();
   const tags = await queries.getAllTags();
   const partsTags = await queries.getPartTags();
-console.log(`fetched categories, tags, and parts`);
-
-
-
+  console.log(`fetched categories, tags, and parts`);
 
   const category_id = await queries.findCategoryId(category_name);
-  console.log(`category_id: ${category_id}`);
+  console.log(`fetched category_id: ${category_id}`);
 
   const parts = await queries.getPartsByCategoryId(category_id);
 
+  // res.send("hello");
+  // If we just send hello and don't render the page below..... nothing weird happens.
 
+  // When we do send the page below it still first logs correctly, then seems to go through everything again this time with a problem.
+  // There's some inception happening
+
+
+  // res.render("../views/pages/individualCategory", {test: "Testing"});
 
 
   res.render("../views/pages/categories", {
@@ -69,12 +74,10 @@ console.log(`fetched categories, tags, and parts`);
     partsTags,
     subTitle: "Categories",
   });
-// Ok, we've gotten parts. Now we need a way to render them. We can reuse our parts display module, but we need to put it into our categories template
-// We might need a new view to handle this, idk gotta think about it
+  // Ok, we've gotten parts. Now we need a way to render them. We can reuse our parts display module, but we need to put it into our categories template
+  // We might need a new view to handle this, idk gotta think about it
 
-// We need a view for specific category
-
-
+  // We need a view for specific category
 };
 // ******************************************************************************************************
 
